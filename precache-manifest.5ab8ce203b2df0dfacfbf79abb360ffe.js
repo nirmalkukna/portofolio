187bd0072a9a7419e83512559e7bfc7492f827c9
nirmalkukna <nirmal.kukna21@gmail.com>
2021-03-16 14:55:08 +0530

    website file upload

---
+++ precache-manifest.5ab8ce203b2df0dfacfbf79abb360ffe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "66aeffd4a0fc24a53de0",
+    "url": "./static/css/main.43e14c14.chunk.css"
+  },
+  {
+    "revision": "66aeffd4a0fc24a53de0",
+    "url": "./static/js/main.167bc3b2.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c4fee72c889736d94977",
+    "url": "./static/css/2.e951c653.chunk.css"
+  },
+  {
+    "revision": "c4fee72c889736d94977",
+    "url": "./static/js/2.89bd121d.chunk.js"
+  },
+  {
+    "revision": "bdee055cd24ecf1fb2f5244db8a60c1d",
+    "url": "./index.html"
+  }
+];
